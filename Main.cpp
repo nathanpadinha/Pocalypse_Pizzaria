d576fa3b1f14ad684568a90d4ab9721592feb935
HeyBobHen <heybobhen@gmail.com>
2026-02-23 11:25:43 -0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -6,17 +6,76 @@ int main()
     InitWindow(800, 600, "Pocalypse Pizzaria");
     
     SetTargetFPS(60);
-    
+
+    // Using Switch Case to determine which screen is running
+    enum gameState {Default, OrderTake, AddToppings, PizzaCook, PizzaCut};
+    gameState currentState = Default;
+    //Default is maybe the intro screen? idk
+
     // Main game loop
     while (!WindowShouldClose())
     {
-        // Update
+        // Just switching through the windows with number keys
+        if (IsKeyPressed(KEY_ONE)) {
+            currentState = Default;
+        }
+        else if (IsKeyPressed(KEY_TWO)) {
+            currentState = OrderTake;
+        }
+        else if (IsKeyPressed(KEY_THREE)) {
+            currentState = AddToppings;
+        }
+        else if (IsKeyPressed(KEY_FOUR)) {
+            currentState = PizzaCook;
+        }
+        else if (IsKeyPressed(KEY_FIVE)) {
+            currentState = PizzaCut;
+        }
+
+
         
-        // Draw
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Pocalypse Pizzaria", 10, 10, 20, BLACK);
-        EndDrawing();
+        switch (currentState) {
+            case Default:
+                // Do default behavior
+                BeginDrawing();
+                ClearBackground(RAYWHITE);
+                DrawText("Default state (intro maybe?)", 10, 10, 20, BLACK);
+                EndDrawing();
+                break;
+            case OrderTake:
+                // Do order taking behavior
+                BeginDrawing();
+                ClearBackground(RAYWHITE);
+                DrawText("Taking in the orders", 10, 10, 20, BLACK);
+                EndDrawing();
+                break;
+            case AddToppings:
+                // Do topping-adding behavior
+                BeginDrawing();
+                ClearBackground(RAYWHITE);
+                DrawText("Topping the toppings!", 10, 10, 20, BLACK);
+                EndDrawing();
+                break;
+            case PizzaCook:
+                // Do pizza cooking behavior
+                BeginDrawing();
+                ClearBackground(RAYWHITE);
+                DrawText("Cookin da pizza", 10, 10, 20, BLACK);
+                EndDrawing();
+                break;
+            case PizzaCut:
+                // do pizza cutting behavior
+                BeginDrawing();
+                ClearBackground(RAYWHITE);
+                DrawText("Chop Chop Chop", 10, 10, 20, BLACK);
+                EndDrawing();
+                break;
+        }
+
+
+
+
+
     }
     
     // De-initialization
