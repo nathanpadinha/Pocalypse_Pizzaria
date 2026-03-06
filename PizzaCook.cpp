aea90077f79fadcd4a56704ade22d8999579d267
Tasha Van Dayne <vandat@sunypoly.edu>
2026-03-06 11:15:03 -0500

    updated timer

--- PizzaCook.cpp
+++ PizzaCook.cpp
@@ -1,4 +1,5 @@
 #include "PizzaCook.hpp"
+#include <string>
 
 /**
  * CookingStage Constructor
@@ -11,16 +12,35 @@ CookingStage::CookingStage() {
     stoveArea.y = 150;
     stoveArea.width = 600;
     stoveArea.height = 400;
+
+    /**
+     * Initialize cooking time values
+     */
+    cookTime = 0.0f;
+    targetCookTime = 10.0f;
+    cooking = true;
 }
 
 /**
  * Update function for the cooking stage
  * @parm dt Delta time between frames
  * * Currently unused but kept for future cooking logic
- * TODO: Use dt to track pizza cooking progress
  */
 void CookingStage::update(float dt) {
-    (void)dt;
+    /**
+     * Increase the cooking timer while pizza is cooking
+     */
+    if(cooking && cookTime < targetCookTime) {
+        cookTime += dt;
+    }
+
+    /**
+     * Stop the timer once the pizza is done
+     */
+    if(cookTime >= targetCookTime) {
+        cookTime = targetCookTime;
+        cooking = false;
+    }
 }
 
 /**
@@ -30,6 +50,31 @@ void CookingStage::update(float dt) {
  */
 void CookingStage::draw() {
     DrawRectangleRec(stoveArea, GRAY);
+
+    /**
+     * Calculate time remaining
+     */
+    int timeLeft = (int)(targetCookTime - cookTime);
+    if(timeLeft < 0) timeLeft = 0;
+
+    /**
+     * Convert timer to string
+     */
+    std::string timerText = "Time Left: " + std::to_string(timeLeft) + "s";
+
+    /**
+     * Draw time above stove
+     */
+    DrawText(timerText.c_str(), 110, 120, 20, BLACK);
+
+    /**
+     * Display cooking status
+     */
+    if(cooking) {
+        DrawText("Status: Cooking", 350, 120, 20, ORANGE);
+    } else {
+        DrawText("Status: Done!", 350, 120, 20, GREEN);
+    }
 }
 
 /**
@@ -38,4 +83,18 @@ void CookingStage::draw() {
  */
 Rectangle CookingStage::getStoveArea() const {
     return stoveArea;
+}
+
+/**
+ * Returns the amount time the pizza has been cooking
+ */
+float CookingStage::getCookTime() const {
+    return cookTime;
+}
+
+/**
+ * Returns whether the pizza has finished cooking
+ */
+bool CookingStage::isFinished() const {
+    return cookTime >= targetCookTime;
 }
